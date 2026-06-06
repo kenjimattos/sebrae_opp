888513f6ce738b768bfb2eeaee3abc60c57be2c5
kenjimattos <kenji.yellow@gmail.com>
2026-06-06 12:34:05 -0300

    refactor: rename SectionCaseStudies to ModeCaseStudies

--- src/components/case-studies/ModeCaseStudies.tsx
+++ src/components/case-studies/ModeCaseStudies.tsx
@@ -8,7 +8,7 @@ import TitleSubtitle from '@/components/ui/TitleSubtitle'
 // CaseStudiesCard w-350 + gap-sm (12px)
 const SCROLL_AMOUNT = 350 + 12
 
-export default function SectionCaseStudies() {
+export default function ModeCaseStudies() {
   return (
     <>
       <section className="flex flex-col gap-lg glass rounded-sm p-lg">
